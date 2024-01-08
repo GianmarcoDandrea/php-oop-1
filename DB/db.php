91c3bcec388d0bb285573495ed7587d0793b3f4e
GianmarcoDandrea <gianm96@hotmail.it>
2024-01-08 16:17:25 +0100

    add: db.php

---
+++ DB/db.php
@@ -0,0 +1,7 @@
+<?php 
+$movies = [
+    new Movie('The Lord Of The Rings - The Return of the King', '200 minutes', ['Fantasy', 'Adventure', 'Action'], 'English', [9, 9, 10 ,10 ,9 ] ,'2003'),
+    new Movie('Van Helsing','145 minutes', ['Fantasy', 'Adventure', 'Horror', 'Action'], 'English', [9, 9, 7 ,7 ,9 ], ),
+    new Movie('Dungeons & Dragons: Honor Among Thieves', '134 minutes', ['Fantasy', 'Adventure', 'Comedy', 'Action'], 'English', [9, 9, 10 ,10 ,9 ] ,'2003')
+];
+?>
--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php 
 require_once __DIR__ . '/Models/Movie.php';
+require_once __DIR__ . '/DB/db.php';
 
-
-$movies = [
-    new Movie('The Lord Of The Rings - The Fellowship Of The Ring', '178 minutes', 'Fantasy', 'English', [9, 8, 8 ,8 ,9 ],'2001'),
-    new Movie('The Lord Of The Rings - The Two Towers', '179 minutes', 'Fantasy', 'English', [9, 9, 7 ,7 ,9 ]),
-    new Movie('The Lord Of The Rings - The Return of the King', '200 minutes', 'Fantasy', 'English', [9, 9, 10 ,10 ,9 ] ,'2003')
-];
 
 var_dump($movies);
 ?>
