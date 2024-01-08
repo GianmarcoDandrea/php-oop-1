dbba4780eb549513f7722749744a2a57bd92c2b1
GianmarcoDandrea <gianm96@hotmail.it>
2024-01-08 16:30:23 +0100

    add: partials

---
+++ partials/footer.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-expand-lg bg-light">
+
+    <div class="container-fluid">
+        <p class=" w-100 text-end">
+            Made by Gianmarco
+        </p>
+    </div>
+</nav>
