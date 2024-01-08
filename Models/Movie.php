43e72c80d5c518c1ec3763d3cd83a70856a81cfb
GianmarcoDandrea <gianm96@hotmail.it>
2024-01-08 15:50:53 +0100

    add: ratings

--- Models/Movie.php
+++ Models/Movie.php
@@ -4,14 +4,21 @@ class Movie {
     public $lenght;
     public $genre;
     public $language;
+    public $ratings;
     public $year;
 
-    function __construct($_name, $_lenght, $_genre, $_language, $_year = "N/A")
+    public function averageRatings($ratings) {
+        $average = array_sum($ratings) / count($ratings);
+        return($average);
+    }
+
+    function __construct($_name, $_lenght, $_genre, $_language, $_ratings, $_year = "N/A")
     {
         $this->name = ucfirst($_name);
         $this->lenght = $_lenght;
         $this->genre = $_genre;
         $this->language = $_language;
+        $this->ratings = $this->averageRatings($_ratings);
         $this->year = $_year;
     }
 }
--- index.php
+++ index.php
@@ -3,9 +3,9 @@ require_once __DIR__ . '/Models/Movie.php';
 
 
 $movies = [
-    new Movie('The Lord Of The Rings - The Fellowship Of The Ring', '178 minutes', 'Fantasy', 'English', '2001'),
-    new Movie('The Lord Of The Rings - The Two Towers', '179 minutes', 'Fantasy', 'English',),
-    new Movie('The Lord Of The Rings - The Return of the King', '200 minutes', 'Fantasy', 'English', '2003')
+    new Movie('The Lord Of The Rings - The Fellowship Of The Ring', '178 minutes', 'Fantasy', 'English', [9, 8, 8 ,8 ,9 ],'2001'),
+    new Movie('The Lord Of The Rings - The Two Towers', '179 minutes', 'Fantasy', 'English', [9, 9, 7 ,7 ,9 ]),
+    new Movie('The Lord Of The Rings - The Return of the King', '200 minutes', 'Fantasy', 'English', [9, 9, 10 ,10 ,9 ] ,'2003')
 ];
 
 var_dump($movies);
